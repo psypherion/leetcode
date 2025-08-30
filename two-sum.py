114fe7791ae7252fc06a80797f82cec1faafe3a6
psypherion <williamskyle562@gmail.com>
2025-08-30 15:23:18 +0530

    Refactor twoSum function to implement O(n) hashmap approach

--- two-sum.py
+++ two-sum.py
@@ -2,19 +2,35 @@ from typing import List
 
 """
 O(n^2) -> Traversing array
-"""
+
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target: 
                     return i, j
 
+"""
+
+
+"""
+O(n) -> Hashmap based approach
+"""
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        storage: dict = {}
+        for i, val in enumerate(nums):
+            diff: int = target - val
+            if diff in storage:
+                return [storage[diff], i]
+            storage[val] = i
+        return []
 
 if __name__ == "__main__":
     sol: object = Solution()
